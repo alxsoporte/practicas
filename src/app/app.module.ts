08ac5cb2ec6c7423d2fb500f00b329f7324e4284
Oscar Cangui <alxserviciotecnico@gmail.com>
2021-11-06 13:43:17 -0500

    se crea el menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { MenuPrincipalComponent } from './components/menu-principal/menu-principal.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent,MenuPrincipalComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  exports: [MenuPrincipalComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
