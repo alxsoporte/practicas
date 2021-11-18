a98eb1f1868258fc86c9009dcdf44e082b111a09
Oscar Cangui <alxserviciotecnico@gmail.com>
2021-11-18 12:53:08 -0500

    creado los menus

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,21 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { MenuPrincipalComponent } from './components/menu-principal/menu-principal.component';
+import { MenusecundarioComponent } from './components/menusecundario/menusecundario.component';
 
 @NgModule({
-  declarations: [AppComponent,MenuPrincipalComponent],
+  declarations: [
+    AppComponent,
+    MenuPrincipalComponent,
+    MenusecundarioComponent
+  ],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
-  exports: [MenuPrincipalComponent],
+  exports: [
+    MenuPrincipalComponent,
+    MenusecundarioComponent
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ src/app/components/menusecundario/menusecundario.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menusecundario',
+  templateUrl: './menusecundario.component.html',
+  styleUrls: ['./menusecundario.component.scss'],
+})
+export class MenusecundarioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
